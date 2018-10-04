9d1e2dea7d2c4638872737ca92a1e148dcee7bea
Javier Quirós Castillo <jquirosc@everis.com>
2018-10-04 19:36:12 +0200

    Initial Commit

---
+++ GITExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITExample
+//
+//  Created by Javier Quiros Castillo on 04/10/2018.
+//  Copyright © 2018 Javier Quiros Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
